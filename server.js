6966323a76a251f15cbb73cfd42d6388df9e7fbd
root <root@Desktop-38.>
2025-04-09 04:34:47 +0000

    Added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/' , (req , res)=>{
+
+   res.send('hello from this server');
+
+});
+
+
+app.listen(port , () =>
+{
+    console.log(`Server listening at http://localhost:${port}`)
+});
